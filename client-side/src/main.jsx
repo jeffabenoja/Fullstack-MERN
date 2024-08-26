51ab1c4c78f1c9ea9bf420bbe64984f682c3622d
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2024-08-27 02:42:11 +0800

    add redux presist

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import "flowbite/dist/flowbite.min.css"
-import { store } from "./redux/store.js"
+import { store, persistor } from "./redux/store.js"
 import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </StrictMode>
 )
--- client-side/src/redux/store.js
+++ client-side/src/redux/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import userReducer from "./user/userSlice"
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import { persistStore } from "redux-persist"
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+// Combine all the individual reducers into a single rootReducer
+const rootReducer = combineReducers({
+  user: userReducer, // Add the user slice reducer
 })
+
+// Configuration object for redux-persist
+const persistConfig = {
+  key: "root", // Key for the storage
+  storage, // Use local storage for persistence
+  version: 1, // Versioning for migration
+}
+
+// Create a persisted reducer using the configuration
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Configure the Redux store with the persisted reducer
+export const store = configureStore({
+  reducer: persistedReducer, // Use the persisted reducer
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check
+    }), // Ensure the middleware array is returned
+})
+
+export const persistor = persistStore(store)
+
+// Note: Ensure you have the necessary packages installed:
+// - @reduxjs/toolkit
+// - redux-persist
+// - redux-persist/lib/storage
