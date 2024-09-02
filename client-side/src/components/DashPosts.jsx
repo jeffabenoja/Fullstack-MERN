6b21aa198ba6cb50e03c93b1a85d2e2a98926b66
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2024-09-02 21:21:27 +0800

    fnished post dashboard

--- client-side/src/components/DashPosts.jsx
+++ client-side/src/components/DashPosts.jsx
@@ -1,5 +1,95 @@
+import { Table } from "flowbite-react"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
 const DashPosts = () => {
-  return <div>DashPost</div>
+  const { currentUser } = useSelector((state) => state.user)
+  const [userPost, setUserPost] = useState([])
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const res = await fetch(`/api/post/getPosts?userId=${currentUser._id}`)
+        const data = await res.json()
+
+        if (res.ok) {
+          setUserPost(data.posts)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    if (currentUser?.isAdmin) {
+      fetchPost()
+    }
+  }, [currentUser._id])
+
+  return (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && userPost.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPost.map((post) => (
+              <Table.Body className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.img}
+                        alt={post.title}
+                        className='w-20 h-10 object-cover bg-gray-500'
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='text-gray-900 font-medium dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell className='capitalize'>
+                    {post.category}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no post yet</p>
+      )}
+    </div>
+  )
 }
 
 export default DashPosts
--- server-side/services/controller/user.js
+++ server-side/services/controller/user.js
@@ -8,6 +8,7 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
+  
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to update this user!"))
   }
