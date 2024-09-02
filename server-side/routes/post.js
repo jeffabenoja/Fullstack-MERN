58a59b50492fddd2b61a9ab73a98a8b2286d00cc
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2024-09-02 15:02:24 +0800

    create api route for getpost

--- server-side/routes/post.js
+++ server-side/routes/post.js
@@ -1,10 +1,10 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { post } from "../services/controller/post.js"
-
+import { post, getPosts } from "../services/controller/post.js"
 
 const router = express.Router()
 
 router.post("/create", verifyToken, post)
+router.get("/getPosts", getPosts)
 
 export default router
