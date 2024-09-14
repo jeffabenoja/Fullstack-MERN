d8529546e9ac77326202b383ea4b8829e2838d66
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2024-09-14 22:58:27 +0800

    finished the comment section

--- client-side/src/components/CommentSection.jsx
+++ client-side/src/components/CommentSection.jsx
@@ -1,7 +1,8 @@
 import { Alert, Button, Textarea } from "flowbite-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import Comment from "./Comment"
 
 const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user)
@@ -38,6 +39,23 @@ const CommentSection = ({ postId }) => {
       setCommentError(error.message)
     }
   }
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getComments/${postId}`)
+
+        if (res.ok) {
+          const data = await res.json()
+          setComments(data)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getComments()
+  }, [postId])
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -90,6 +108,22 @@ const CommentSection = ({ postId }) => {
             </Alert>
           )}
         </form>
+      )}
+
+      {comments.length === 0 ? (
+        <p className='text-sm my-5'>No comments yet!</p>
+      ) : (
+        <>
+          <div className='text-sm my-5 flex items-center gap-1'>
+            <p>Comments</p>
+            <div className='border border-gray-400 py-1 px-2 rounded-sm '>
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
       )}
     </div>
   )
--- server-side/routes/comment.js
+++ server-side/routes/comment.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createComment } from "../services/controller/comment.js"
+import { createComment, getComments } from "../services/controller/comment.js"
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router()
 
 router.post("/create", verifyToken, createComment)
+router.get("/getComments/:postId", getComments)
 
 export default router
